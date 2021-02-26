276f481712fc78e165c31b34443dad8d2f6725c6
yakarim <attar4443@gmail.com>
2021-02-26 11:09:33 +0700

    home

--- controller/home.go
+++ controller/home.go
@@ -10,7 +10,7 @@ import (
 func (c Ctrl) Index(ctx *atreugo.RequestCtx) error {
 
 	return c.HTML(ctx, 200, "pages/index", cfg.H{
-		"title":  "Home",
+		"title":  "Home walid",
 		"user":   database.GetSession(ctx, "Username"),
 		"signIn": database.SessionAuth(ctx),
 	})
